c7e0f06049ca34b04a47ea550924e22ce4dbcef3
awasevalentine <awasevalentine@GMAIL.com>
2020-10-17 15:10:28 +0100

    Added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LgaService } from './core/services/lga.service';
 import { PUnitService } from './core/services/punit.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'p@ssword001',
+      database: 'bincomphptest',
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController, LGAController, PollingUnitController],
   providers: [AppService, LgaService, PUnitService],
 })
