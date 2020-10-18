81fec60d533cf57ca4a30cb3cf748b21d353dfeb
awasevalentine <awasevalentine@GMAIL.com>
2020-10-18 16:56:26 +0100

    Added endpoint form fetching wards in an LGA

--- src/api/controllers/lga.controller.ts
+++ src/api/controllers/lga.controller.ts
@@ -1,6 +1,7 @@
 import { LgaService } from './../../core/services/lga.service';
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Ward } from 'src/core/domain/entities/entity.ward';
 
 @Controller("api/lgas")
 @ApiTags("LGAs")
@@ -17,5 +18,9 @@ export class LGAController {
   @Get(":id/results")
   async  getLGAResults(@Param('id') lgaId: number): Promise<any[]> {
       return this._lgaService.getLgaResult(lgaId);
+  }
+  @Get(":lgaId/wards")
+  async getWards(@Param('lgaId') lgaId:number): Promise<Ward[]> {
+    return this._lgaService.getWardsInLGA(lgaId);
   }
 }
--- src/core/services/lga.service.ts
+++ src/core/services/lga.service.ts
@@ -4,14 +4,17 @@ import { In, Repository } from 'typeorm';
 import { LGA } from '../domain/entities/entity.lga';
 import { PollingUnit } from '../domain/entities/entity.polingunit';
 import { PollingUnitResult } from '../domain/entities/entity.pu_result';
+import { Ward } from '../domain/entities/entity.ward';
 
 @Injectable()
 export class LgaService {
   
+  
     private readonly _logger: LoggerService = new Logger();
     constructor(@InjectRepository(PollingUnit) private _puRepository: Repository<PollingUnit>,
         @InjectRepository(LGA) private _lgaRepository: Repository<LGA>, 
-        @InjectRepository(PollingUnitResult) private _puResultRepository: Repository<PollingUnitResult>
+        @InjectRepository(PollingUnitResult) private _puResultRepository: Repository<PollingUnitResult>,
+        @InjectRepository(Ward) private _wardRepository: Repository<Ward>
     ) {
 
     }
@@ -29,5 +32,9 @@ export class LgaService {
       const puIds = pollingUnitsInLGA.map(p=> p.uniqueid);
       const lgaResults = await this._puResultRepository.find({PUnitId: In(puIds)});
       return lgaResults;
+  }
+
+  async getWardsInLGA(lgaId: number): Promise<Ward[]> {
+    return this._wardRepository.find({where: {LgaId: lgaId}});
   }
 }
